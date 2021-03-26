b606aa6264a12f95532ce06583f24186de58e65b
voltkrugers <80946089+voltkrugers@users.noreply.github.com>
2021-03-26 09:58:58 +0100

    etudiant2

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -10,7 +10,44 @@
 
 
 //Fonctions réalisées par l'étudiant 2 :
+bool estPair(int nb)
+{
+	if (nb%2=0)
+	{
+		return true;
+	}else
+	{
+		return false;
+	}
+}
 
+int sommeNombre(int nb)
+{
+	int resultat = 0;
+	int i=0;
+	for(i=0;i<nb;i++)
+	{
+		resultat=resultat+nb;
+	}
+	return resultat;
+}
+
+float salaireNet(int salaire)
+{
+	int resultat;
+	resultat=salaire*0.77;
+	return resultat;
+}
+
+int plusPetit(int a, int b, int c)
+{
+	int resultat;
+	if(a<b && a<c)
+	{
+		return a;
+	}
+	
+}
 
 
 
