942fef3406244558fcb647ac8551e9d7550df17b
Zortekxe <81406991+Zortekxe@users.noreply.github.com>
2021-04-01 20:56:01 +0200

    Update TD_GitHub.cpp

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,14 +1,59 @@
 //Fonctions réalisées par l'étudiant 1 :
+bool estMajeur(int age)
+{
+	bool resultat;
 
+	if(age >= 18)
+	{
+		resultat=true;
+	}
+	else
+	{
+		resultat=false;
+	}
+	return resultat;
+}
 
+int exposant(int nbre, int exp)
+{
+	int resultat, i;
 
+	for (i=0;i<=exp;i++)
+	{
+		resultat = resultat * nombre;
+	}
+	return resultat;
+	
+}
 
+float TVA(int prix)
+{
+	float resultat;
 
+	resultat = prix * 0.804;
 
+	return resultat;
 
+}
 
+int plusGrand(int a, int b, int c)
+{
+	int resultat;
 
-
+	if(a>b && a>c)
+	{
+		resultat=a;
+	}
+	else if(b>c && b>a)
+	{
+		resultat=b;
+	}
+	else if(c>a && c>b)
+	{
+		resultat=c;
+	}
+	return resultat;
+}
 //Fonctions réalisées par l'étudiant 2 :
 bool estPair(int nb)
 {
